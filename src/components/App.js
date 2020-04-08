4ca22f1c3dcc5384e32d73437393b2d6191ec1fb
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-08 16:22:12 -0400

    Boilerplate setup for project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import './style.css';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+export default App;
